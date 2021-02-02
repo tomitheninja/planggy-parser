fdbc8c47e197e4a0cc5387b69e29972045f54b1c
Südi Tamás <suditomi@pm.me>
2021-02-02 19:35:53 +0100

    ✨ Implement a lot of operations

--- src/ast.rs
+++ src/ast.rs
@@ -9,10 +9,10 @@ pub enum Constant {
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Operation {
-    Parentheses(Box<Expression>),             // (exp)
-    Abs(Box<Expression>),                     // |exp|
-    ItemAt(Box<Expression>),                  // arr[idx]
-    Slice(Box<Expression>, Box<Expression>),  // arr[start..end]
+    Parentheses(Box<Expression>), // (exp)
+    Abs(Box<Expression>),         // |exp|
+    // ItemAt(Box<Expression>),                  // arr[idx]
+    // Slice(Box<Expression>, Box<Expression>),  // arr[start..end]
     Search(Box<Expression>, Box<Expression>), // arr @ item
     Not(Box<Expression>),                     // NEM <exp:boolean>
     Neg(Box<Expression>),                     // - <exp:boolean>
@@ -29,14 +29,15 @@ pub enum Operation {
     Round(Box<Expression>),                   // KEREK <exp:float>
     ToUpper(Box<Expression>),                 // NAGY <exp:char>
     ToLower(Box<Expression>),                 // KIS <exp:char>
-    IsWordChar(Box<Expression>),              // BETU <exp:char>
-    IsDigit(Box<Expression>),                 // SZAM <exp:char>
+    IsWordChar(Box<Expression>),              // BETU <exp:char> -> matches [a-zA-Z]?
+    IsDigit(Box<Expression>),                 // SZAM <exp:char> -> matches [0-9]?
     Pow(Box<Expression>, Box<Expression>),    // <exp:float> ^ <exp:float>
     Mul(Box<Expression>, Box<Expression>),    // <exp:int|float> * <exp:int|float>
     Div(Box<Expression>, Box<Expression>),    // <exp:float> / <exp:float>
     DivInt(Box<Expression>, Box<Expression>), // <exp:int> DIV <exp:int>
     Mod(Box<Expression>, Box<Expression>),    // <exp:int> MOD <exp:int>
-    Add(Box<Expression>, Box<Expression>),    // <exp:int|float|string> + <exp|int|string&char>
+    Add(Box<Expression>, Box<Expression>),    // <exp:int|float|string> + <exp:int|string&char>
+    Sub(Box<Expression>, Box<Expression>),    // <exp:int|float> + <exp:int|float>
     And(Box<Expression>, Box<Expression>),    // <exp:logical> ES <exp:logical>
     Or(Box<Expression>, Box<Expression>),     // <exp:logical> VAGY <exp:logical>
     Eq(Box<Expression>, Box<Expression>),     // <exp:any> = <exp:any>
