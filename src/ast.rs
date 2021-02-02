7cedb4bf99d3c55b63c56b8a0849427fcb69a41e
Südi Tamás <suditomi@pm.me>
2021-02-02 19:08:30 +0100

    📝 Write documentation for all operations

--- src/ast.rs
+++ src/ast.rs
@@ -9,15 +9,48 @@ pub enum Constant {
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Operation {
-    Parentheses(Box<Expression>),
-    Not(Box<Expression>),
-    Neg(Box<Expression>),
-    Mul(Box<Expression>, Box<Expression>),
-    Add(Box<Expression>, Box<Expression>),
+    Parentheses(Box<Expression>),             // (exp)
+    Abs(Box<Expression>),                     // |exp|
+    ItemAt(Box<Expression>),                  // arr[idx]
+    Slice(Box<Expression>, Box<Expression>),  // arr[start..end]
+    Search(Box<Expression>, Box<Expression>), // arr @ item
+    Not(Box<Expression>),                     // NEM <exp:boolean>
+    Neg(Box<Expression>),                     // - <exp:boolean>
+    Rnd(Box<Expression>),                     // RND <exp:int ~ upper limit>
+    Sin(Box<Expression>),                     // SIN <exp:float ~ radian>
+    Cos(Box<Expression>),                     // COS <exp:float ~ radian>
+    Tan(Box<Expression>),                     // TAN <exp:float ~ radian>
+    Arcsin(Box<Expression>),                  // SIN <exp:float ~ radian>
+    Arccos(Box<Expression>),                  // SIN <exp:float ~ radian>
+    Log(Box<Expression>),                     // SIN <exp:float>
+    Exp(Box<Expression>),                     // SIN <exp:float>
+    ToFloat(Box<Expression>),                 // VALOS <exp:int>
+    Floor(Box<Expression>),                   // EGESZ <exp:float>
+    Round(Box<Expression>),                   // KEREK <exp:float>
+    ToUpper(Box<Expression>),                 // NAGY <exp:char>
+    ToLower(Box<Expression>),                 // KIS <exp:char>
+    IsWordChar(Box<Expression>),              // BETU <exp:char>
+    IsDigit(Box<Expression>),                 // SZAM <exp:char>
+    Pow(Box<Expression>, Box<Expression>),    // <exp:float> ^ <exp:float>
+    Mul(Box<Expression>, Box<Expression>),    // <exp:int|float> * <exp:int|float>
+    Div(Box<Expression>, Box<Expression>),    // <exp:float> / <exp:float>
+    DivInt(Box<Expression>, Box<Expression>), // <exp:int> DIV <exp:int>
+    Mod(Box<Expression>, Box<Expression>),    // <exp:int> MOD <exp:int>
+    Add(Box<Expression>, Box<Expression>),    // <exp:int|float|string> + <exp|int|string&char>
+    And(Box<Expression>, Box<Expression>),    // <exp:logical> ES <exp:logical>
+    Or(Box<Expression>, Box<Expression>),     // <exp:logical> VAGY <exp:logical>
+    Eq(Box<Expression>, Box<Expression>),     // <exp:any> = <exp:any>
+    Ne(Box<Expression>, Box<Expression>),     // <exp:any> /= <exp:any>
+    Lt(Box<Expression>, Box<Expression>),     // <exp:any> < <exp:any>
+    Le(Box<Expression>, Box<Expression>),     // <exp:any> <= <exp:any>
+    Gt(Box<Expression>, Box<Expression>),     // <exp:any> > <exp:any>
+    Ge(Box<Expression>, Box<Expression>),     // <exp:any> >= <exp:any>
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Expression {
     Const(Constant),
     Op(Operation),
+    // TODO:
+    // Variable(Variable)
 }
