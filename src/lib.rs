17a82e21fa1dc651fab2b779351953beae55a61b
Südi Tamás <suditomi@pm.me>
2021-02-02 22:11:42 +0100

    🔥 Remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 #[macro_use]
 extern crate lalrpop_util;
 
-#[cfg(test)]
-mod tests;
-
 pub mod ast;
 
 lalrpop_mod!(pub planggy);
 
-pub use planggy::InputParser as Parser;
+#[cfg(test)]
+mod tests;
