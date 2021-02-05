325fcb7726784f1de6b53eac5ff2ae5fe4e3eed2
Südi Tamás <suditomi@pm.me>
2021-02-05 17:02:25 +0100

    Create property accessors for Variable

--- vars-parser/src/variable.rs
+++ vars-parser/src/variable.rs
@@ -6,3 +6,13 @@ pub struct Variable {
     name: VarName,
     its_type: VarType,
 }
+
+impl Variable {
+    pub fn name(&self) -> &VarName {
+        &self.name
+    }
+
+    pub fn its_type(&self) -> &VarType {
+        &self.its_type
+    }
+}
